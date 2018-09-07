d800c62f41380210707f07d4d44a596b7140bc72
Tommi Reiman <tommi@iki.fi>
2018-09-07 14:04:58 +0300

    Add api-tests

--- test/clj/myapp/test/handler.clj
+++ test/clj/myapp/test/handler.clj
@@ -2,7 +2,12 @@
   (:require [clojure.test :refer :all]
             [ring.mock.request :refer :all]
             [myapp.handler :refer :all]
+            [myapp.muuntaja :as muuntaja]
+            [muuntaja.core :as m]
             [mount.core :as mount]))
+
+(defn parse-json [body]
+  (m/decode muuntaja/instance "application/json" body))
 
 (use-fixtures
   :once
@@ -18,4 +23,13 @@
 
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
-      (is (= 404 (:status response))))))
+      (is (= 404 (:status response)))))
+
+  (testing "api-route"
+    (testing "success"                 
+      (let [response (app (request :get "/api/math/plus" {:x 1, :y 2}))]
+        (is (= 200 (:status response)))  
+        (is (= {:total 3} (-> response :body parse-json)))))
+    (testing "failure"
+      (let [response (app (request :get "/api/math/plus" {:x 1, :y "kikka"}))]
+        (is (= 400 (:status response)))))))
