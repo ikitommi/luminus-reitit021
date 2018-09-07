6c56c31c2aa9b5ac7e598b6b6ee2f91b6b203801
Tommi Reiman <tommi@iki.fi>
2018-09-07 13:42:50 +0300

    New service-routes

--- src/clj/myapp/routes/services.clj
+++ src/clj/myapp/routes/services.clj
@@ -1,29 +1,84 @@
 (ns myapp.routes.services
-  (:require [muuntaja.middleware :as muuntaja]
-            [reitit.ring.coercion :as rrc]
-            [reitit.swagger :as swagger]
+  (:require [reitit.swagger :as swagger]
+            [reitit.ring.coercion :as coercion]
+            [reitit.ring.schema :as ring-schema]
+            [reitit.coercion.schema :as schema-coercion]
             [ring.util.http-response :refer :all]
-            [ring.middleware.params :as params]))
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [reitit.ring.middleware.exception :as exception]
+            [reitit.ring.middleware.multipart :as multipart]
+            [myapp.middleware :as middleware]
+            [ring.middleware.params :as params]
+            [schema.core :as s]
+            [clojure.java.io :as io]))
 
 (defn service-routes []
   ["/api"
-   {:middleware [params/wrap-params
-                 muuntaja/wrap-format
-                 swagger/swagger-feature
-                 rrc/coerce-exceptions-middleware
-                 rrc/coerce-request-middleware
-                 rrc/coerce-response-middleware]
-    :swagger {:id ::api
-              :info {:title "my-api"
-                     :description "using [reitit](https://github.com/metosin/reitit)."}
-              :produces #{"application/json"
-                          "application/edn"
-                          "application/transit+json"}
-              :consumes #{"application/json"
-                          "application/edn"
-                          "application/transit+json"}}}
+   {:middleware [;; query-params & form-params
+                 params/wrap-params
+                 ;; content-negotiation
+                 muuntaja/format-negotiate-middleware
+                 ;; encoding response body
+                 muuntaja/format-response-middleware
+                 ;; exception handling
+                 exception/exception-middleware
+                 ;; decoding request body
+                 muuntaja/format-request-middleware
+                 ;; coercing response bodys
+                 coercion/coerce-response-middleware
+                 ;; coercing request parameters
+                 coercion/coerce-request-middleware
+                 ;; multipart
+                 multipart/multipart-middleware]
+    ;; schema-coercion
+    :coercion schema-coercion/coercion
+    ;; muuntaja instance to for body encoding & decoding
+    :muuntaja middleware/muuntaja}
+
    ["/swagger.json"
     {:get {:no-doc true
+           :swagger {:info {:title "my-api"
+                            :description "with reitit-ring"}}
            :handler (swagger/create-swagger-handler)}}]
-   ["/ping" {:get (constantly (ok {:message "ping"}))}]
-   ["/pong" {:post (constantly (ok {:message "pong"}))}]])
+
+   ["/ping"
+    {:get (constantly (ok {:message "ping"}))}]
+
+   ["/files"
+    {:swagger {:tags ["files"]}}
+
+    ["/upload"
+     {:post {:summary "upload a file"
+             :parameters {:multipart {:file ring-schema/TempFilePart}}
+             :responses {200 {:body {:name s/Str, :size s/Int}}}
+             :handler (fn [{{{:keys [file]} :multipart} :parameters}]
+                        {:status 200
+                         :body {:name (:filename file)
+                                :size (:size file)}})}}]
+
+    ["/download"
+     {:get {:summary "downloads a file"
+            :swagger {:produces ["image/png"]}
+            :handler (fn [_]
+                       {:status 200
+                        :headers {"Content-Type" "image/png"}
+                        :body (-> "public/img/warning_clojure.png"
+                                  (io/resource)
+                                  (io/input-stream))})}}]]
+
+   ["/math"
+    {:swagger {:tags ["math"]}}
+
+    ["/plus"
+     {:get {:summary "plus with query parameters"
+            :parameters {:query {:x s/Int, :y s/Int}}
+            :responses {200 {:body {:total s/Int}}}
+            :handler (fn [{{{:keys [x y]} :query} :parameters}]
+                       {:status 200
+                        :body {:total (+ x y)}})}
+      :post {:summary "plus with body parameters"
+             :parameters {:body {:x s/Int, :y s/Int}}
+             :responses {200 {:body {:total s/Int}}}
+             :handler (fn [{{{:keys [x y]} :body} :parameters}]
+                        {:status 200
+                         :body {:total (+ x y)}})}}]]])
